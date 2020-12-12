ffd8aa94db17a407004667d49166f2630512257f
MBanawa <mikebanawa25@gmail.com>
2020-12-13 07:46:01 +0800

    added place model, great places provider and imagepicker

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:great_places/models/place.dart';
 
@@ -6,5 +7,16 @@ class GreatPlaces with ChangeNotifier {
 
   List<Place> get items {
     return [..._items];
+  }
+
+  void addPlace(String pickedTitle, File pickedImage) {
+    final newPlace = Place(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
