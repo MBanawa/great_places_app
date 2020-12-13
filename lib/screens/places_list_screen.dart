1b70586c2d2517d69781c8125aed3bf0121e0d55
MBanawa <mikebanawa25@gmail.com>
2020-12-13 11:08:45 +0800

    Add Place Detail screen and Open in Map feature

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/screens/place_detail_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:great_places/screens/add_place_screen.dart';
 import 'package:great_places/providers/great_places.dart';
@@ -41,7 +42,10 @@ class PlacesListScreen extends StatelessWidget {
                           title: Text(greatPlaces.items[i].title),
                           subtitle: Text(greatPlaces.items[i].location.address),
                           onTap: () {
-                            //go to detail page...
+                            Navigator.of(context).pushNamed(
+                              PlaceDetailScreen.routeName,
+                              arguments: greatPlaces.items[i].id,
+                            );
                           },
                         ),
                       ),
